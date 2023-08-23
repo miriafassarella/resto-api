403ac9fab47a5b6a83767d95444acd52441658bb
Miriã Fassarella <miriafassarella@gmail.com>
2023-08-23 11:02:12 -0400

    create and config database

---
+++ src/main/java/com/mabrasoft/restoapi/RestoApiApplication.java
@@ -0,0 +1,13 @@
+package com.mabrasoft.restoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mabrasoft/restoapi/RestoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabrasoft.restoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
