8ef840a5920bf9327b7e950d7f3bb11ca28a41dd
Miriã Fassarella <miriafassarella@gmail.com>
2023-08-23 19:55:22 -0400

    searching a kitchen with the controller

--- src/main/java/com/mabrasoft/restoapi/api/controller/KitchenController.java
+++ src/main/java/com/mabrasoft/restoapi/api/controller/KitchenController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,12 @@ public class KitchenController {
 	@ResponseStatus(code = HttpStatus.OK)
 	public List<Kitchen> kitchenList(){
 		return kitchenRepository.list();
+	}
+	
+	@GetMapping("/{kitchenId}")
+	public ResponseEntity<Kitchen> kitchenSearch(@PathVariable Long kitchenId){
+		Kitchen kitchen =  kitchenRepository.search(kitchenId);
+		 return ResponseEntity.status(HttpStatus.FOUND).body(kitchen);
 	}
 	
 }
--- src/main/resources/Import.sql
+++ src/main/resources/Import.sql
@@ -1,1 +1,4 @@
 insert into kitchen (name) values ('Thai');
+insert into kitchen (name) values ('Brazilian');
+insert into kitchen (name) values ('American');
+insert into kitchen (name) values ('Indian');
